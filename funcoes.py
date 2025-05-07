3a458dcb3eca24d15a0327090f54542725f1a2b5
JoaoNery08 <joaoalnf@al.insper.edu.br>
2025-05-06 22:24:58 -0300

    Exercício 2

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,14 @@
+#Exercício 1
 import random
 
 def rolar_dados(quantidade):
  
     return [random.randint(1, 6) for _ in range(quantidade)]
+
+#Exercício 2
+def guardar_dado(dados_rolados, dados_no_estoque, indice):
+    
+    if 0 <= indice < len(dados_rolados):
+        dado = dados_rolados.pop(indice)
+        dados_no_estoque.append(dado)
+    return [dados_rolados, dados_no_estoque]
