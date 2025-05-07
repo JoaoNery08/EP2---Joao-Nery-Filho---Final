58648fdebd7839176f716ce50e7304d7f9afcded
JoaoNery08 <joaoalnf@al.insper.edu.br>
2025-05-06 22:33:45 -0300

    Exercício 3

--- funcoes.py
+++ funcoes.py
@@ -6,16 +6,22 @@ def rolar_dados(quantidade):
     return [random.randint(1, 6) for _ in range(quantidade)]
 
 #Exercício 2
+
 def guardar_dado(dados_rolados, dados_no_estoque, indice):
 
     if 0 <= indice < len(dados_rolados):
-        dado = dados_rolados[indice]
-        nova_lista_rolados = dados_rolados[:indice] + dados_rolados[indice+1:]
-        nova_lista_estoque = dados_no_estoque + [dado]
-        return [nova_lista_rolados, nova_lista_estoque]
-    
-    else:
-        return [dados_rolados, dados_no_estoque]
+        dado = dados_rolados.pop(indice) #para retornar ou remover um indice específico de uma lista, nesse caso o que se recebe da função
+        dados_no_estoque.append(dado)
+    return [dados_rolados, dados_no_estoque]
+
+#Exercício 3
+
+def remover_dado(dados_rolados, dados_no_estoque, indice):
+
+    if 0 <= indice < len(dados_no_estoque):
+        dado = dados_no_estoque.pop(indice)
+        dados_rolados.append(dado)
+    return [dados_rolados, dados_no_estoque]
 
 
 
